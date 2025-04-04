a1054d9e93311087a1636e7e988611074fc4976b
Leon Steinbach <lest9540@colorado.edu>
2025-04-04 13:39:55 -0600

    altered /register test case for better page detection

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -35,9 +35,8 @@ describe('Server!', () => {
 const html_start_regex = /^<!DOCTYPE html>.*/;
 const html_end_regex = /.*<\/html>$/;
 
-const register_regex = /.*<form action=.* method=.*>.*/
-
 // Test cases for /register API, positive and negative
+const register_regex = /.*<h2>Log In<\/h2>.*/
 describe('Testing Register User API', () => {
     it('positive : /register', done => {
       chai
@@ -46,7 +45,7 @@ describe('Testing Register User API', () => {
         .send({username: 'John Doe', password: 'scoobydoo'})
         .end((err, res) => {
           expect(res).to.have.status(200);
-          check = (html_start_regex.test(res.text) && html_end_regex.test(res.text) && register_regex.test(res.text))
+          check = (html_start_regex.test(res.text) && html_end_regex.test(res.text) && register_regex.test(res.text));
           assert(check == true);
           done();
         });
@@ -59,7 +58,6 @@ describe('Testing Register User API', () => {
         .send({username: undefined, password: 'scoobydoo'})
         .end((err, res) => {
           expect(res).to.have.status(400);
-          console.log("\nres.text:\n" + res.text + "\n");
           done();
         });
     });
@@ -71,25 +69,24 @@ describe('Testing Register User API', () => {
 
 // ********************************************************************************
 
-// // Testing for Redirect
-// describe('Testing Redirect', () => {
-//   // Sample test case given to test /test endpoint.
-//   it('\test route should redirect to /login with 302 HTTP status code', done => {
-//     chai
-//       .request(server)
-//       .get('/test')
-//       .end((err, res) => {
-//         res.should.have.status(302); // Expecting a redirect status code
-//         res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/); // Expecting a redirect to /login with the mentioned Regex
-//         done();
-//       });
-//   });
-// });
+// Testing for Redirect
+describe('Testing Redirect', () => {
+  it('\test route should redirect to /login with 302 HTTP status code', done => {
+    chai
+      .request(server)
+      .get('/test')
+      .end((err, res) => {
+        console.log("\n===================\n" + JSON.stringify(res) + "\n===================\n");
+        res.should.have.status(200);
+        res.should.redirectTo(/^.*127\.0\.0\.1.*\/login/); // Expecting a redirect to /login with the mentioned Regex
+        done();
+      });
+  });
+});
 
 
 // Testing for Render
 describe('Testing Render', () => {
-  // Sample test case given to test /test endpoint.
   it('test "/login" route should render with an html response', done => {
     chai
       .request(server)
