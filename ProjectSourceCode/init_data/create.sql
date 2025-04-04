e969bf9e86699a1cc544dd3b8dfdb826d430fbbb
gunkfar <gjbauer528@gmail.com>
2025-04-04 11:18:32 -0600

    added "add transaction" button and start on backend for transaction table

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -3,3 +3,13 @@ CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(60) NOT NULL
 );
+SELECT * FROM users;
+
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    final_balance DECIMAL(10, 2) NOT NULL,
+    transaction_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
