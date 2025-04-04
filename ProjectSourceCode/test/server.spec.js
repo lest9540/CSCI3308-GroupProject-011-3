a74722b5aab60854b164f22e328a35572cad3406
Nhan Do <nhdo3427@colorado.edu>
2025-04-03 19:15:13 -0600

    Lab 11 Part 13 Copy Paste Redirect/Render Test

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -71,4 +71,33 @@ describe('Testing Register User API', () => {
 
 // ********************************************************************************
 
-// Testing for Redirect and Render
+// Testing for Redirect
+describe('Testing Redirect', () => {
+  // Sample test case given to test /test endpoint.
+  it('\test route should redirect to /login with 302 HTTP status code', done => {
+    chai
+      .request(server)
+      .get('/test')
+      .end((err, res) => {
+        res.should.have.status(302); // Expecting a redirect status code
+        res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/); // Expecting a redirect to /login with the mentioned Regex
+        done();
+      });
+  });
+});
+
+
+// Testing for Render
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/login" route should render with an html response', done => {
+    chai
+      .request(server)
+      .get('/login') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  });
+});
