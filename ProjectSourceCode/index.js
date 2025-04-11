ce9c3481d49145ca1ca2eaf34f2ed4ab2aec0d7b
Leon Steinbach <lest9540@colorado.edu>
2025-04-11 08:32:40 -0600

    mailgun API setup start

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -1,3 +1,6 @@
+// import FormData from "form-data"; // form-data v4.0.1
+// import Mailgun from "mailgun.js"; // mailgun.js v11.1.0
+
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
@@ -8,6 +11,8 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const axios = require('axios');
+const nodemailer = require('nodemailer');
+const mail = require('mailgun.js');
 
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
@@ -64,7 +69,26 @@ const auth = (req, res, next) => {
     next();
 };
 
-// API Routes //
+// API Funcs //
+async function sendSimpleMessage() {
+  const mailgun = new mail(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.API_KEY || "API_KEY",
+  });
+  try {
+    const data = await mg.messages.create("sandboxa2db92420e4b4b1b985d93f3f2e6d247.mailgun.org", {
+      from: "Mailgun Sandbox <postmaster@sandboxa2db92420e4b4b1b985d93f3f2e6d247.mailgun.org>",
+      to: ["Leon Steinbach <lest9540@colorado.edu>"],
+      subject: "Spell Saver API Test Email",
+      text: "Congratulations Leon Steinbach, you just sent an email with Mailgun! You are truly awesome!",
+    });
+
+    console.log(data); // logs response data
+  } catch (error) {
+    console.log(error); //logs any error
+  }
+}
 
 app.get('/welcome', (req, res) => {
   res.json({status: 'success', message: 'Welcome!'});
@@ -80,6 +104,7 @@ app.get('/login', (req, res) => {
 // .get for the front page
 app.get('/front', (req, res) => {
   res.render('pages/front');
+  // sendSimpleMessage();
 });
 
 //---------------------------------------------------------------------------------------------
@@ -136,28 +161,28 @@ app.get('/settings', (req, res) => {
     res.render('pages/settings')
 });
 
-app.get('/discover', (req, res) => {
-    axios({
-        url: `https://app.ticketmaster.com/discovery/v2/events.json`,
-        method: 'GET',
-        dataType: 'json',
-        headers: { 'Accept-Encoding': 'application/json'},
-        params: {
-            apikey: process.env.API_KEY,
-            keyword: 'Muse',
-            size: 10
-        }
-    })
-    .then(results => { 
-        // the results will be displayed on the terminal if the docker containers are running 
-        res.render('pages/discover', {event: results.data._embedded.events});
-        // Send some parameters
-    })
-    .catch(error => { 
-        console.log(error);
-        res.render(400, 'pages/discover', {event: []});
-    });
-});
+// app.get('/discover', (req, res) => {
+//     axios({
+//         url: `https://app.ticketmaster.com/discovery/v2/events.json`,
+//         method: 'GET',
+//         dataType: 'json',
+//         headers: { 'Accept-Encoding': 'application/json'},
+//         params: {
+//             apikey: process.env.API_KEY,
+//             keyword: 'Muse',
+//             size: 10
+//         }
+//     })
+//     .then(results => { 
+//         // the results will be displayed on the terminal if the docker containers are running 
+//         res.render('pages/discover', {event: results.data._embedded.events});
+//         // Send some parameters
+//     })
+//     .catch(error => { 
+//         console.log(error);
+//         res.render(400, 'pages/discover', {event: []});
+//     });
+// });
 
 app.get('/logout', (req, res) => {
     req.session.destroy();
