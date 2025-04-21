3e4d70707805f47b04ddf44edb22fafdb5e0e69c
Leon Steinbach <lest9540@colorado.edu>
2025-04-21 12:51:54 -0600

    assmebled summary messages

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -94,6 +94,73 @@ async function sendOptInMessage(name, email) {
   }
 }
 
+async function assembleSummaryText(name, callback) {
+  try {
+    return await db.any('SELECT * FROM transactions WHERE user_id = $1', [name])
+    .then(results => {
+      var recent_text = "Here are all transactions so far this month:\n"; // assemble list of recent transactions
+      var upcoming_text = "Here are all upcoming transactions this month:\n"; // assemble list of upcoming transactions
+      var cur_date = new Date();
+      var cur_day = cur_date.getDate();
+      var cur_month = cur_date.getMonth() + 1; // Months are zero-based
+      var temp_month = undefined;
+      var temp_day = undefined;
+      // ignoring the year for scope of project
+
+      for (let i = 0; i < results.length; i++) { // find all transactions this month
+        temp_month = results[i].transaction_date.getMonth() + 1;
+        temp_day = results[i].transaction_date.getDate();
+
+        if ((cur_month != temp_month) || (temp_day > cur_day)) continue; // only summarizing this month up to today
+        
+        recent_text += results[i].name + " - " + results[i].transaction_date.toLocaleDateString() + " - $" + results[i].amount + "\n";
+      }
+
+      for (let i = 0; i < results.length; i++) { // find all upcoming transactions this month
+        temp_month = results[i].transaction_date.getMonth() + 1;
+        temp_day = results[i].transaction_date.getDate();
+
+        if ((cur_month != temp_month) || (temp_day <= cur_day)) continue; // only summarizing this month after today
+        
+        upcoming_text += results[i].name + " - " + results[i].transaction_date.toLocaleDateString() + " - $" + results[i].amount + "\n";
+      }
+
+      console.log("Greetings " + name + ",\n\nBecause you have opted into regular updates we have provided you the following summary for this month so far:\n\n" + recent_text + "\n" + upcoming_text + "\n Thank you for using SpellSaver!");
+      callback("Greetings " + name + ",\n\nBecause you have opted into regular updates we have provided you the following summary for this month so far:\n\n" + recent_text + "\n" + upcoming_text + "\n Thank you for using SpellSaver!");
+      // return ("Greetings " + name + ",\n\nBecause you have opted into regular updates we have provided you the following summary for this month so far:\n\n" + recent_text + "\n" + upcoming_text + "\n Thank you for using SpellSaver!");
+    })
+    .catch(error => {
+      console.log(error);
+      return undefined;
+    });
+  }
+  catch(error) {
+    console.log(error);
+    return undefined;
+  };
+};
+
+async function sendSummary(name, email, summary_text) {
+  const mailgun = new mail(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.API_KEY || "API_KEY",
+  });
+  try {
+    const data = await mg.messages.create("sandboxa2db92420e4b4b1b985d93f3f2e6d247.mailgun.org", {
+      from: "SpellSaver <lest9540@colorado.edu>",
+      to: [name + " <" + email + ">"],
+      subject: "Thank You for Optin In",
+      text: summary_text,
+    });
+
+    console.log(data);
+  }
+  catch (error){
+    console.log(error);
+  }
+};
+
 app.get('/welcome', (req, res) => {
   res.json({status: 'success', message: 'Welcome!'});
 });
@@ -166,6 +233,10 @@ app.get('/user', async (req, res) => {
 
 app.get('/settings', (req, res) => {
   res.render('pages/settings', {OptIn: req.session.user[0].reminders});
+  assembleSummaryText(req.session.user[0].username, text => {
+    console.log(text);
+    sendSummary(req.session.user[0].username, req.session.user[0].email, text);
+  })
 });
 
 app.post('/settings', async (req, res) => {
