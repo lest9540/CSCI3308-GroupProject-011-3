19f36bf282093127c176f77be9c84bfb3fd9f6ae
gunkfar <gjbauer528@gmail.com>
2025-04-22 16:05:06 -0600

    (maybe) fixed index.js

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -8,7 +8,10 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const axios = require('axios');
-
+const nodemailer = require('nodemailer');
+const mail = require('mailgun.js');
+const schedule = require('node-schedule');
+const wt = require("worker-thread");
 
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
@@ -24,6 +27,8 @@ const dbConfig = {
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
+  email: process.env.POSTGRES_EMAIL,
+  reminders: process.env.POSTGRES_REMINDERS,
 };
 
 const db = pgp(dbConfig);
@@ -52,7 +57,6 @@ app.use(
   })
 );
 
-
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -67,7 +71,111 @@ const auth = (req, res, next) => {
     next();
 };
 
-// API Routes //
+// API Funcs //
+async function sendOptInMessage(name, email) {
+  const mailgun = new mail(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.API_KEY || "API_KEY",
+  });
+  try {
+    const data = await mg.messages.create("sandboxa2db92420e4b4b1b985d93f3f2e6d247.mailgun.org", {
+      from: "SpellSaver <lest9540@colorado.edu>",
+      to: [name + " <" + email + ">"],
+      subject: "Thank You for Optin In",
+      text: "Congratulations " + name + ",\n\n       You have succesfully opted into regular updates about your budget with SpellSaver.\n       We will be sending you regular updates about your budget.\n\n Thank you for using SpellSaver!",
+    });
+
+    console.log(data); // logs response data
+  } catch (error) {
+    console.log(error); //logs any error
+  }
+}
+
+async function assembleSummaryText(name, callback) {
+  try {
+    return await db.any('SELECT * FROM transactions WHERE user_id = $1', [name])
+    .then(results => {
+      var recent_text = "Here are all transactions so far this month:\n"; // assemble list of recent transactions
+      var upcoming_text = "Here are all upcoming transactions this month:\n"; // assemble list of upcoming transactions
+      var cur_date = new Date();
+      var cur_day = cur_date.getDate();
+      var cur_month = cur_date.getMonth() + 1; // Months are zero-based
+      var temp_month = undefined;
+      var temp_day = undefined;
+      // ignoring the year for scope of project
+
+      for (let i = 0; i < results.length; i++) { // find all transactions this month
+        temp_month = results[i].transaction_date.getMonth() + 1;
+        temp_day = results[i].transaction_date.getDate();
+
+        if ((cur_month != temp_month) || (temp_day > cur_day)) continue; // only summarizing this month up to today
+        
+        recent_text += results[i].name + " - " + results[i].transaction_date.toLocaleDateString() + " - $" + results[i].amount + "\n";
+      }
+
+      for (let i = 0; i < results.length; i++) { // find all upcoming transactions this month
+        temp_month = results[i].transaction_date.getMonth() + 1;
+        temp_day = results[i].transaction_date.getDate();
+
+        if ((cur_month != temp_month) || (temp_day <= cur_day)) continue; // only summarizing this month after today
+        
+        upcoming_text += results[i].name + " - " + results[i].transaction_date.toLocaleDateString() + " - $" + results[i].amount + "\n";
+      }
+
+      callback("Greetings " + name + ",\n\nBecause you have opted into regular updates we have provided you the following summary for this month so far:\n\n" + recent_text + "\n" + upcoming_text + "\n Thank you for using SpellSaver!");
+    })
+    .catch(error => {
+      console.log(error);
+      return undefined;
+    });
+  }
+  catch(error) {
+    console.log(error);
+    return undefined;
+  };
+};
+
+async function sendSummary(name, email, summary_text) {
+  const mailgun = new mail(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.API_KEY || "API_KEY",
+  });
+  try {
+    const data = await mg.messages.create("sandboxa2db92420e4b4b1b985d93f3f2e6d247.mailgun.org", {
+      from: "SpellSaver <lest9540@colorado.edu>",
+      to: [name + " <" + email + ">"],
+      subject: "Thank You for Optin In",
+      text: summary_text,
+    });
+
+    console.log(data);
+  }
+  catch (error){
+    console.log(error);
+  }
+};
+
+async function summary() {
+  console.log("summary function called");
+  try {
+    await db.any('SELECT * FROM users WHERE reminders = true')
+      .then(results => {
+        for (let i = 0; i < results.length; i++) {
+          assembleSummaryText(results[i].username, text => {
+            sendSummary(results[i].username, results[i].email, text);
+          })
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
 app.get('/welcome', (req, res) => {
   res.json({status: 'success', message: 'Welcome!'});
@@ -98,7 +206,7 @@ app.post("/login", async (req, res) => {
             if (match) { // found user and password
                 req.session.user = user;
                 req.session.save();
-                res.redirect('/banking');
+                res.redirect('/user');
             }
             else { // found user wrong password
                 res.render('pages/login.hbs')
@@ -114,55 +222,59 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const hash = await bcrypt.hash(req.body.password, 10);
-  db.none('INSERT INTO users(username, password, email) VALUES($1, $2, $3)', [req.body.username, hash, req.body.email])
+  db.none('INSERT INTO users(username, password, email) VALUES($1, $2, $3)', [req.body.name, hash, req.body.email])
     .then(() => {
       res.redirect('/login');
     })
     .catch(error => {
       console.log(error);
-      res.redirect(400, '/register');
+      res.redirect('/register');
     });
 });
-
-// Authentication Required past here
-app.use(auth);
 
 app.get('/', (req, res) => {
     res.redirect('/login');
 });
 
-app.get('/user', (req, res) => {
-  res.render('pages/user')
+// Authentication Required past here
+app.use(auth);
+
+app.get('/user', async (req, res) => {
+  var flag = req.session.user[0].reminders;
+  if (flag == undefined) { // default off value is technically undefined
+    flag = false;
+  }
+  res.render('pages/user', {username: req.session.user[0].username, email: req.session.user[0].email, OptIn: flag});
 });
 
 app.get('/settings', (req, res) => {
-    res.render('pages/settings')
+  res.render('pages/settings', {OptIn: req.session.user[0].reminders});
 });
 
-app.get('/discover', (req, res) => {
-    axios({
-        url: `https://app.ticketmaster.com/discovery/v2/events.json`,
-        method: 'GET',
-        dataType: 'json',
-        headers: { 'Accept-Encoding': 'application/json'},
-        params: {
-            apikey: process.env.API_KEY,
-            keyword: 'Muse',
-            size: 10
-        }
+app.post('/settings', async (req, res) => {
+  var flag = req.body.EmailOptIn;
+  if (flag == undefined) { // default off value is technically undefined
+    flag = false;
+  }
+  else if (flag == 'on') {
+    flag = true;
+  }
+
+  db.any('UPDATE users SET reminders = $1 WHERE username = $2', [flag, req.session.user[0].username])
+    .then(() => {
+      if (flag) {
+        sendOptInMessage(req.session.user[0].username, req.session.user[0].email);
+      }
+      req.session.user[0].reminders = flag;
+      res.render('pages/user', {name: req.session.user[0].user, email: req.session.user[0].email, OptIn: flag});
     })
-    .then(results => { 
-        // the results will be displayed on the terminal if the docker containers are running 
-        res.render('pages/discover', {event: results.data._embedded.events});
-        // Send some parameters
-    })
-    .catch(error => { 
-        console.log(error);
-        res.render(400, 'pages/discover', {event: []});
+    .catch(error => {
+      console.log(error);
+      res.render('pages/user', {name: req.session.user[0].user, email: req.session.user[0].email, OptIn: flag});
     });
-});
+  });
 
-app.get('/banking', async (req, res) => {
+  app.get('/banking', async (req, res) => {
     try{
       let results = await db.any('SELECT * FROM transactions WHERE user_id = $1', [req.session.user[0].username]);
       res.render('pages/banking', {transactions: results});
