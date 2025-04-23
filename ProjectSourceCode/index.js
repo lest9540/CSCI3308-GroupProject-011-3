ca38d442089ace4a2a6722c3dd0b0237b37f4198
Leon Steinbach <lest9540@colorado.edu>
2025-04-22 20:50:29 -0600

    change back

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -20,7 +20,7 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: 'users_db_m3br',
+  host: 'db',
   port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
