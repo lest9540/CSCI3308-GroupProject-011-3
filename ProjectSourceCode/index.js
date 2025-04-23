eb46202631b39def0bf895ce55e4b4a6c134d036
Nhan Do <nhdo3427@colorado.edu>
2025-04-23 01:09:02 -0600

    Typo Fix

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -20,7 +20,7 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: process.env.POSRGRES_HOST || 'db',
+  host: process.env.POSTRGRES_HOST || 'db',
   port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
