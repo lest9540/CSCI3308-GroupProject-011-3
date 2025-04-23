2d159687d62e6d1d74cbf4e2fca54e24ff88ce10
gunkfar <gjbauer528@gmail.com>
2025-04-22 18:47:58 -0600

    fixed date formatting

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -193,6 +193,10 @@ app.post('/settings', async (req, res) => {
 });
 
 app.post('/addTransaction', (req, res) => {
+  const date = new Date(req.body.transactionDate);
+  const formattedDate = date.toISOString().split('T')[0]; // Format date to YYYY-MM-DD
+  req.body.transactionDate = formattedDate;
+  db.none('INSERT INTO transactions(user_id, name, category, transaction_date, amount, final_balance) VALUES($1, $2, $3, $4, $5, $6)', [req.session.user[0].username, req.body.transactionName, req.body.category, formattedDate, req.body.transactionAmount, req.body.finalBalance]);
   db.none('INSERT INTO transactions(user_id, name, category, transaction_date, amount, final_balance) VALUES($1, $2, $3, $4, $5, $6)', [req.session.user[0].username, req.body.transactionName, req.body.category, req.body.transactionDate, req.body.transactionAmount, req.body.finalBalance]);
   res.redirect('/banking');
 });
