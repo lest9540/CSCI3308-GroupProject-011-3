d9c115186f9f88df084e6e1d3fc2776759bcf88c
Leon Steinbach <lest9540@colorado.edu>
2025-04-22 21:16:56 -0600

    adding possible host to env

--- ProjectSourceCode/timer.js
+++ ProjectSourceCode/timer.js
@@ -19,7 +19,7 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: 'db',
+  host: process.env.POSTGRES_HOST || 'db',
   port: 5432,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
